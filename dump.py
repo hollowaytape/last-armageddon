9fc3e7fbaf563bc19f9acf9bd9d398c2db454732
hollowaytape <hollowaytape@retro-type.com>
2018-10-25 17:53:07 -0400

    Dump to an excel sheet

--- dump.py
+++ dump.py
@@ -2,11 +2,24 @@
     Text dumper for Last Armageddon.
     Based on a .tbl file which interprets characters.
 """
-
+import xlsxwriter
 from rominfo import ORIGINAL_DATA_TRACK, SEGMENTS
+
+class Dumpstring:
+    def __init__(self, string, segment, loc):
+        self.string = string
+        self.segment = segment
+        self.loc = loc
+
+
+workbook_FILENAME = 'LA_Text.xlsx'
+
+workbook = xlsxwriter.Workbook(workbook_FILENAME)
+header = workbook.add_format({'bold': True, 'align': 'center', 'bottom': True, 'bg_color': 'gray'})
 
 if __name__ == '__main__':
     TABLE = {}
+    sjis_strings = []
     with open('LA.tbl', 'rb') as f:
         for line in f.readlines():
             try:
@@ -26,22 +39,88 @@ if __name__ == '__main__':
                 buf = b''
                 while cursor < len(seg):
                     b = seg[cursor]
-                    #print(b)
-                    #print(TABLE)
-                    # TODO: If it's a breath mark, change the most recent character.
+
+                    # <END> control code. End the buffer
                     if b == 0:
                         if len(buf) > 2:
                             print(hex(cursor), buf)
                             loc = hex(cursor + s.start - len(buf)).encode()
                             g.write(s.name.encode() + b' ' + loc + b' ' + buf + b'\n')
+                            sjis_strings.append(Dumpstring(buf, s, cursor))
                         buf = b''
+                    # ゛ increases the SJIS index of the previous char by 1
+                    elif b == 0x9e or b == 0xde:
+                        if buf:
+                            buf = buf[:-1] + (buf[-1] + 1).to_bytes(1, 'little')
+                    # ﾟ does it by 2
+                    elif b == 0x9f or b == 0xdf:
+                        if buf:
+                            buf = buf[:-1] + (buf[-1] + 2).to_bytes(1, 'little')
+                    # The normal case
                     elif b in TABLE:
                         buf += TABLE[b]
+                    # Something else. End the buffer
                     else:
                         if len(buf) > 2:
                             print(hex(cursor), buf)
                             loc = hex(cursor + s.start - len(buf)).encode()
                             g.write(s.name.encode() + b' ' + loc + b' ' + buf + b'\n')
+                            sjis_strings.append(Dumpstring(buf, s, cursor))
                         buf = b''
                     cursor += 1
 
+    worksheet = workbook.add_worksheet("LA")
+    for s in sjis_strings:
+
+        worksheet.set_column('A:A', 20)
+        worksheet.write(0, 0, 'Offset', header)
+
+        worksheet.write(0, 1, 'Offset (Seg)', header)
+
+        # Block column should be narrow
+        worksheet.set_column('C:C', 10)
+        worksheet.write(0, 2, 'Segment', header)
+
+        # JP column should be wide
+        worksheet.set_column('D:D', 30)
+        worksheet.write(0, 3, 'Japanese', header)
+
+        # JP_LEN column
+        worksheet.set_column('E:E', 5)
+        worksheet.write(0, 4, 'JP_Len', header)
+
+        # EN column
+        worksheet.set_column('F:F', 30)
+        worksheet.write(0, 5, 'English', header)
+
+        # EN_LEN column
+        worksheet.set_column('G:G', 5)
+        worksheet.write(0, 6, 'EN_Len', header)
+
+        # Comments column
+        worksheet.write(0, 7, 'Comments', header)
+        row = 1
+        for s in sjis_strings:
+
+            total_loc = '0x' + hex(s.loc + s.segment.start).lstrip('0x').zfill(8)
+            loc = '0x' + hex(s.loc).lstrip('0x').zfill(3)
+            segment = str(s.segment.name)
+            jp = s.string.decode('shift_jis_2004')
+
+
+            worksheet.write(row, 0, total_loc)
+            worksheet.write(row, 1, loc)
+            worksheet.write(row, 2, segment)
+            worksheet.write(row, 3, jp)
+
+            # Also write JP to the EN column, per kuoushi request
+            #worksheet.write(row, 6, jp)
+
+            # Add the JP/EN length formulas.
+            # TODO: Get a regex for this to ignore bracketed stuff
+            # Excel can't do regex like GSheets...
+            worksheet.write(row, 4, "=LEN(D%s)" % str(row+1))
+            worksheet.write(row, 6, "=LEN(F%s)" % str(row+1))
+            row += 1
+
+    workbook.close()
--- rominfo.py
+++ rominfo.py
@@ -19,41 +19,44 @@ class Segment:
         return hex(self.iso_start)[2:].zfill(8)
 
 # Offsets of locations in track2.bin (89,227 KB, headered).
+
+# TODO: Segments that are images or SJIS text can be subclasses.
+        # They shouldn't get dumped by the dumper.
 SEGMENTS = [
-    Segment(0x60c5, 0x63f0, "Names1"),
-    Segment(0x6520, 0x69e5, "Names2"),
+    # TODO: One of these things broke stuff when I reinserted everything. Bit off more than I should have at once...
+    #Segment(0x60c5, 0x63f0, "Names1"),
+    #Segment(0x6520, 0x69e5, "Names2"),
     #Segment(0x9310, 0x9b10, "Encyclo-1"),  # One sector of encyclopedia
     #Segment(0x13aab, 0x13ae6, "MainMenu"),  # Uses SJIS
-    Segment(0x151de, 0x152d0, "NamesA"),
-    Segment(0x15400, 0x15afe, "NamesB"),
-    Segment(0x16f90, 0x174d7, "Debug"),
+    #Segment(0x151de, 0x152d0, "NamesA"),
+    #Segment(0x15400, 0x15afe, "NamesB"),
+    #Segment(0x16f90, 0x174d7, "Debug"),
     #Segment(0x25e70, 0x26670, "Encyclo-2"),
 
     #Segment(0x55473c0, 0x554bbf0, "FontImg"),
 
-    Segment(0x5517af5, 0x5517de4, "Dunno5"),
+    #Segment(0x5517af5, 0x5517de4, "Dunno5"),
 
-    Segment(0x5518fe4, 0x5519040, "Spells1"),
-    Segment(0x55194a0, 0x55198f0, "Spells2"),
+    #Segment(0x5518fe4, 0x5519040, "Spells1"),
+    #Segment(0x55194a0, 0x55198f0, "Spells2"),
 
-    Segment(0x5526ef0, 0x5526f33, "Combat1"),
+    #Segment(0x5526ef0, 0x5526f33, "Combat1"),  # maybe Combat2 is the right one?
 
-    #Segment(0x5528b90, 0x5529390, "Dunno1"),  # No text here?
-    Segment(0x5529b8f, 0x5529de0, "Dunno2"),
+    #Segment(0x5529b8f, 0x5529de0, "Dunno2"),
 
     Segment(0x554ec88, 0x554eccd, "Combat2"),
 
-    Segment(0x555260d, 0x5552a30, "Dunno3"),
+    #Segment(0x555260d, 0x5552a30, "Dunno3"),
 
-    Segment(0x56090f5, 0x5609116, "Menu"),
-    Segment(0x561be27, 0x561be44, "Submenu1"),
-    Segment(0x561c5cb, 0x561c670, "Submenu2"),
-    Segment(0x561d1e4, 0x561d290, "Submenu3"),
-    Segment(0x561d9db, 0x561da2f, "Submenu4"),
+    Segment(0x56090f5, 0x560911b, "Menu"),
+    #Segment(0x561be27, 0x561be45, "Submenu1"),  # this one definitely crashes.
+    #Segment(0x561c5cb, 0x561c670, "Submenu2"),
+    #Segment(0x561d1e4, 0x561d290, "Submenu3"),
+    #Segment(0x561d9db, 0x561da2f, "Submenu4"),
 
-    Segment(0x561f3c6, 0x55529a6, "Dunno4"),
+    #Segment(0x561f3c6, 0x55529a6, "Dunno4"),
 
-    Segment(0x561fb50, 0x561fcbd, "Weapons"),
+    #Segment(0x561fb50, 0x561fcbd, "Weapons"),
 ]
 
 # Font is in iso at 0x5cd37a0-0x5cd7fe0.
